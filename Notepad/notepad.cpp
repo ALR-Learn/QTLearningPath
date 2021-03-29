6e7effed57b931c2586d34fc906e9d5ecaa0e14c
Allan Regush <17693494+AllanRegush@users.noreply.github.com>
2021-03-28 19:09:36 -0600

    Notepad

---
+++ Notepad/notepad.cpp
@@ -0,0 +1,93 @@
+#include "notepad.h"
+#include "ui_notepad.h"
+
+Notepad::Notepad(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::Notepad)
+{
+    ui->setupUi(this);
+    this->setCentralWidget(ui->textEdit);
+
+    QFont font;
+    font.setFamily("Courier");
+    font.setStyleHint(QFont::Monospace);
+    font.setFixedPitch(true);
+    font.setPointSize(14);
+    ui->textEdit->setFont(font);
+
+    constexpr int tabStop = 4;
+    QFontMetrics metrics(font);
+    ui->textEdit->setTabStopDistance(tabStop * metrics.width(' '));
+
+    connect(ui->actionNew, &QAction::triggered, this, &Notepad::newDocument);
+    connect(ui->actionOpen, &QAction::triggered, this, &Notepad::open);
+    connect(ui->actionSave, &QAction::triggered, this, &Notepad::save);
+    connect(ui->actionSave_As, &QAction::triggered, this, &Notepad::saveAs);
+}
+
+Notepad::~Notepad()
+{
+    delete ui;
+}
+
+void Notepad::newDocument()
+{
+    currentFile.clear();
+    ui->textEdit->setText(QString());
+}
+
+void Notepad::open()
+{
+    QString fileName = QFileDialog::getOpenFileName(this, "Open file");
+    QFile file(fileName);
+    currentFile = fileName;
+    if (!file.open(QIODevice::ReadOnly | QFile::Text)) {
+        QMessageBox::warning(this, "Warning", "Cannot open file: " + file.errorString());
+        return;
+    }
+
+    setWindowTitle(fileName);
+    QTextStream in(&file);
+    QString text = in.readAll();
+    ui->textEdit->setText(text);
+}
+
+void Notepad::save()
+{
+    QString fileName;
+    if (currentFile.isEmpty()) {
+        fileName = QFileDialog::getSaveFileName(this, "Save");
+        currentFile = fileName;
+    } else {
+        fileName = currentFile;
+    }
+    QFile file(fileName);
+    if (!file.open(QIODevice::WriteOnly | QFile::Text)) {
+        QMessageBox::warning(this, "Warning", "Cannot save file: " + file.errorString());
+        return;
+    }
+    setWindowTitle(fileName);
+    QTextStream out(&file);
+    QString text = ui->textEdit->toPlainText();
+    out << text;
+    file.close();
+}
+
+void Notepad::saveAs()
+{
+    QString fileName = QFileDialog::getSaveFileName(this, "Save as");
+    QFile file(fileName);
+
+    if (!file.open(QFile::WriteOnly | QFile::Text)) {
+        QMessageBox::warning(this, "Warning", "Cannot save file:" + file.errorString());
+        return;
+    }
+    currentFile = fileName;
+    setWindowTitle(fileName);
+    QTextStream out(&file);
+    QString text = ui->textEdit->toPlainText();
+    out << text;
+    file.close();
+}
+
+
